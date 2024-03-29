549c3cb6f5575159d6095a371a51ebabede2e2f9
Nandasagarre <160044611+Crank-Newt@users.noreply.github.com>
2024-03-29 12:14:04 +0530

    layer1

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ router.get("/resister", (req, res)=>{
     res.render("resister");
 })
 
+router.get("/gatesyllab", (req, res) =>{
+    res.render("gatesyllab")
+})
+
+router.get("/psusyllab",(req,res)=>{
+    res.render("psusyllab")
+})
+
 module.exports = router;
